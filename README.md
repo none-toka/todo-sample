2d740e884bb709a1cb69f162cc39cf9f98294977
naokata <snaokata@gmail.com>
2019-10-12 19:02:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TOTD sample
+
+See https://qiita.com/EBIHARA_kenji/items/25e59f7132b96cb886f3
+
+## History
+
+### 4日目
+
---
+++ ts/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux'; // 追加
+import UserForm from './components/UserForm'; // 追加
+import Store from './Store'; // 追加
+
+const container = document.getElementById('contents');
+// 変更 -->
+ReactDom.render(
+    <Provider store={Store}>
+        <UserForm />
+    </Provider>,
+    container,
+);
